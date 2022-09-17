413533948d72f56315d1cbfa486b9b30bf9daae4
caa789 <caa789@naver.com>
2022-09-18 02:23:13 +0900

    기존 Table View -> RxCocoa 적용 후 간단하게 개선, 코드 모듈화 개선

--- IntroduceBodyBuilding/ViewController.swift
+++ IntroduceBodyBuilding/ViewController.swift
@@ -8,7 +8,7 @@ import RxDataSources
 
 class ViewController: UIViewController{
     let disposeBag = DisposeBag()
- 
+    
     @IBOutlet weak var mainTableView: UITableView!
     
     struct cellData{ // 셀 데이터 및 뷰 컨트롤러 데이터 저장 구조체 (메인 cell, detail 페이지 -> 2개의 컬렉션으로 구성)
@@ -18,29 +18,27 @@ class ViewController: UIViewController{
     }
     
     func MainTVSet(){
-        mainTableView.delegate = self
-        mainTableView.dataSource = self
         mainTableView.separatorStyle = .none
         mainTableView.showsVerticalScrollIndicator = false
     }
     
     func makeFireStoreData(){
-        readData(collection: "Program", model: cellData.mainVCModel)
-        readData(collection: "Detail", model: cellData.detailVCModel)
-    }
-    
-    func readData<T: Decodable>(collection: String, model: [T]) {
-        Firestore.firestore().collection(collection).getDocuments() { (querySnapshot, err) in //메인페이지 데이터 Read
-            if let err = err {
-                print("Error getting documents: \(err)")
-            } else {
-                for document in querySnapshot!.documents { //data -> json 변환
-                    guard let data = try? JSONSerialization.data(withJSONObject: document.data(), options: []) else{ return }
-                    if let decode = try? JSONDecoder().decode(T.self, from: data){ //json 데이터를 struct에.set
-                        applyModel(data: decode)
+        readData(collection: "Program", model: cellData.mainVCModel) // Program -> 메인 셀 데이터
+        readData(collection: "Detail", model: cellData.detailVCModel) // Detail -> 상세 뷰 데이터
+        
+        func readData<T: Decodable>(collection: String, model: [T]) {
+            Firestore.firestore().collection(collection).getDocuments() { (querySnapshot, err) in //메인페이지 데이터 Read
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents { //data -> json 변환
+                        guard let data = try? JSONSerialization.data(withJSONObject: document.data(), options: []) else{ return }
+                        if let decode = try? JSONDecoder().decode(T.self, from: data){ //json 데이터를 struct에 set
+                            applyModel(data: decode)
+                        }
                     }
+                    tableViewBinding(collection: collection) // 메인 테이블 뷰 바인딩 (상세 뷰는 메인 Cell 데이터를 가져오기 때문)
                 }
-                self.mainTableView.reloadData()
             }
         }
         
@@ -52,9 +50,17 @@ class ViewController: UIViewController{
                 cellData.detailVCModel.append(data as! DetailVCModel)
             }
         }
+        
+        func tableViewBinding(collection: String){
+            if collection == "Program" { // 메인 테이블 뷰 데이터 로딩 -> 메인 테이블 뷰 바인딩
+                self.bindTableView(isFilterd: false)
+            }
+        }
+        
+        
     }
     
-    var isFiltering: Bool {
+    var isFiltering: Bool{
         let searchController = self.navigationItem.searchController
         let isActive = searchController?.isActive ?? false
         let isSearchBarHasText = searchController?.searchBar.text?.isEmpty == false //서치바에 텍스트가 존재 시 true
@@ -80,7 +86,6 @@ class ViewController: UIViewController{
     }
     
     func moveVC <T>(name: String, VC: T, indexPath: IndexPath?) {
-        
         if VC is DetailViewController{
             let storyboard = UIStoryboard(name: name, bundle: nil).instantiateViewController(withIdentifier: name) as! DetailViewController
             storyboard.titleName = cellData.detailVCModel[indexPath!.row].title
@@ -89,12 +94,10 @@ class ViewController: UIViewController{
             storyboard.url = cellData.detailVCModel[indexPath!.row].url
             self.navigationController?.pushViewController(storyboard, animated: true)
         }
-        
         else if VC is MyProgramViewController{
             let storyboard = UIStoryboard(name: name, bundle: nil).instantiateViewController(withIdentifier: name) as! MyProgramViewController
             self.present(storyboard, animated: true)
         }
-        
     }
     
     func makeButton() {
@@ -104,7 +107,7 @@ class ViewController: UIViewController{
             let basketButton = UIButton()
             setButton()
             selectAction()
-
+            
             func selectAction(){
                 basketButton.rx.tap.bind { _ in
                     self.moveVC(name: "MyProgramViewController", VC: MyProgramViewController(), indexPath: nil)
@@ -128,66 +131,46 @@ class ViewController: UIViewController{
                 basketButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -18).isActive = true
             }
         }
-
+        
     }
-
+    private func bindTableView(isFilterd: Bool) {
+        mainTableView.delegate = nil
+        mainTableView.dataSource = nil
+        
+        let mainOb: Observable<[MainVCModel]> = Observable.of(cellData.mainVCModel) //메인 테이블 뷰
+        let filteredOb: Observable<[MainVCModel]> = Observable.of(cellData.filteredModel) // 검색 활성화 시 출력되는 테이블 뷰
+        
+        isFilterd ? bindingCell(observable: filteredOb) : bindingCell(observable: mainOb)
+        
+        setupTableViewDelegate()
+        
+        func bindingCell(observable: Observable<[MainVCModel]>){
+            observable.bind(to: self.mainTableView.rx.items(cellIdentifier: "MainVCCell", cellType: MainVCCell.self)) { (index: Int, element: MainVCModel, cell: MainVCCell) in
+                
+                cell.titleLabel.text = element.title
+                cell.authorLabel.text = element.author
+                cell.descriptionLabel.text = element.description
+                cell.recommendLabel.text = element.recommend
+                cell.divisionLabel.text = element.division
+                cell.healthImageView.image = UIImage(named: element.image)
+            }.disposed(by: self.disposeBag)
+        }
+        
+        func setupTableViewDelegate() {
+            mainTableView.rx.itemSelected
+                .bind(onNext: { indexPath in
+                    self.mainTableView.deselectRow(at: indexPath, animated: true)
+                    self.moveVC(name: "DetailViewController", VC: DetailViewController(), indexPath: indexPath)
+                }).disposed(by: disposeBag)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         makeFireStoreData()
         makeSearchBar()
         MainTVSet()
         makeButton()
-    }
-}
-
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        if isFiltering { //검색 활성화 -> 검색 데이터 적용, 검색 비활성화 -> 기존 데이터 사용
-            return cellData.filteredModel.count
-        }
-        else{
-            return cellData.mainVCModel.count
-        }
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let cell = mainTableView.dequeueReusableCell(withIdentifier: "MainVCCell", for: indexPath)
-        as! MainVCCell
-
-        if isFiltering { //검색 활성화 -> 검색 데이터 적용, 검색 비활성화 -> 기존 데이터 사용
-            bindingCell(cell: cell, model: cellData.filteredModel , indexPath: indexPath)
-        }
-        else{
-            bindingCell(cell: cell, model: cellData.mainVCModel, indexPath: indexPath)
-        }
-        
-        return cell
-        
-        func bindingCell(cell: MainVCCell, model: [MainVCModel], indexPath: IndexPath) {
-                        
-            cell.titleLabel.text = model[indexPath.row].title
-            cell.authorLabel.text = model[indexPath.row].author
-            cell.descriptionLabel.text = model[indexPath.row].description
-            cell.recommendLabel.text = model[indexPath.row].recommend
-            cell.divisionLabel.text = model[indexPath.row].division
-            cell.healthImageView.image = UIImage(named: model[indexPath.row].image)
-        }
-    }
-    
-
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 170
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
-        tableView.deselectRow(at: indexPath, animated: true)
-
-        self.moveVC(name: "DetailViewController", VC: DetailViewController(), indexPath: indexPath)
     }
 }
 
@@ -198,9 +181,6 @@ extension ViewController: UISearchResultsUpdating{
             MainVCModel.title.contains(text) || MainVCModel.author.contains(text) ||
             MainVCModel.description.contains(text) || MainVCModel.division.contains(text)
         })
-        mainTableView.reloadData()
-        }
+        isFiltering ? bindTableView(isFilterd: true) : bindTableView(isFilterd: false)
     }
-
-
-
+}
